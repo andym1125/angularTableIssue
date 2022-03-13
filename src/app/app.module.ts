98c7fb337aefe157d28abaca464f2c42b8ba6577
Andy M <andym1125@gmail.com>
2022-03-12 20:44:45 -0600

    only commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MycompComponent } from './mycomp/mycomp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MycompComponent
   ],
   imports: [
     BrowserModule,
